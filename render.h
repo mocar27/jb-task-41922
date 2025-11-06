723b2f5acbbfacd1692ed18773b7129c1c4453f3
mocar27 <komocarski@gmail.com>
2025-11-06 16:30:52 +0100

    decode gfx_render function

--- render.h
+++ render.h
@@ -5,8 +5,6 @@
 
 #include <stdio.h>
 
-#define EXIT_SUCCESS 0
-
 /** 
  * Arguments to functions are passed in following order of registers
  * rdi, rsi, rdx, rcx, r8, r9
@@ -17,30 +15,25 @@
  * Float arguments are passed in separate floating-point registers (%xmm{..}).
 
  * Ints are 32 bits registers - eax, ebx, ecx, edx, esi, edi.
- * Longs/pointers are 64 bits - rax, rbx, rcx, rdx, rsi, rdi. *
+ * Longs/pointers are 64 bits - rax, rbx, rcx, rdx, rsi, rdi.
  */
-
-// time i sleep sa pewnie potrzebne zeby to okno od razu sie nie zamknelo
 
 // gfx_context has size of 1072 and it makes sense, since in gfx_close there is a call to address stored in this struct
 // with the offset of 1064 (428h in gfx_close reference), this value is assigned in gfx_init_context function
 typedef struct gfx_context {
-    char* name;                     // in the offset 0 (from init_context read of addresses)
+    uint64_t name;                  // in the offset 0 (from init_context read of addresses, holds the name casted to uint64_t)
     int32_t width;                  // in the offset 8 (from init_context read of addresses)
     int32_t height;                 // in the offset 12 (from init_context read of addresses)
 
+    int* framebuffer;               // in the offset 16 (gfx_render), potentially switch to uint8_t*
     // if we can't find any more arguments, do padding with unused bytes
-    uint8_t padding[1040];          // 1072 - 32 = 1040 bytes of padding to fill the struct to 1072 bytes
+    // uint8_t padding[1048];       // 1072 - 8 - 4 - 4 - 900 - 8 = 1048 bytes of padding to fill the struct to 1072 bytes
     uint64_t window_addr;           // in the offset 1064 (0x428) of this struct to later use in gfx_close (from gfx_close read of addresses)
 } gfx_context;
 
 // gfx_loop                     -- void, ktoremu podaje sie 1 argument (pewnie z tym calym contextem) -- potem okno zaincjalizowane w loopie rysuje animacje
 // gfx_render
 // gfx_create_context
-void gfx_loop(void* arg);
-// data to prawdopodobnie context, a ten int jest nic niewarty, bo potem (juz po wyprintowaniu "You did it!")
-// sprawdzane jest czy on jest 0, jesli tak to dzieje sie cos jeszcze, jesli nie to program sie konczy - moze to jest animacja ta z bonusa?
-// void gfx_render(void* data, int ); -- void, ktoremu podaje sie 2 argumenty --  render na koniec / przed zamknieciem okna
 
 /** 
  * Since we multiply two numbers by each other and then by 4, it's likely 
@@ -65,8 +58,9 @@ void gfx_close(void* ctx);
  * In our decoded assembly c=0, n=1072 and str is pointer to gfx_context,
  * so our context struct will have exactly 1072 bytes of later used memory
  * (created window pop-up is addressed by pointer at offset 1064 of this struct).
+ * Before calling this function, ctx has to already be allocated in memory.
  */
-void gfx_create_context(void* ctx, int32_t width, int32_t height, char* name);
+void* gfx_create_context(void* ctx, int32_t width, int32_t height, uint64_t name_addr);
 
 /** 
  * Functions, that all work the same way.
@@ -74,10 +68,13 @@ void gfx_create_context(void* ctx, int32_t width, int32_t height, char* name);
  * - gfx_get_height_screen returns 300
  * - gfx_get_width_screen returns 300
  * - gfx_get_window_title returns "Challenge"
+ * Since gfx_get_window_title returns a pointer to a string,
+ * we're safe to read it and return it as int64_t.
+ * Later if wanted to output as string, we would read from beginning of that address.
  */
 int32_t gfx_get_height_screen(void);
 int32_t gfx_get_width_screen(void);
-char* gfx_get_window_title(void);
+int64_t gfx_get_window_title(void);
 
 /** 
  * High-level function, that based on input pointer to the context struct, 
@@ -88,17 +85,40 @@ char* gfx_get_window_title(void);
  */
 void gfx_init_context(void* ctx);
 
+/**
+ * 
+ */
+void gfx_loop(void* ctx);
+
+/**
+ * Function reads input arguments and based on the height of the window
+ * calculates the position that "You did it! :D" will later be displayed.
+ * Then it loops over int* framebuffer until iterator of the loop 
+ * isn't equal to width*height and fills every framebuffer[i] 
+ * (pixel, every element has 4 bytes) with color value 0x101010 (which is dark gray).
+ * When the loop ends, function jumps to section in which the draw_text is called
+ * with passing gfx_context pointer, string "You did it! :D", render position, 
+ * calculated height and color in which message will be displayed (0xFF00 - bright green).
+ * If the flag is set to 0, function returns after drawing the text.
+ * If not, then based on function making operation of floating points 
+ * implements looping horizontal motion for the text on the screen.
+ * (My deduction is, that it's related to the `Bonus` mentioned in the task description).
+ */
+void gfx_render(void* ctx, int flag);
+
 /** 
  * Takes 1 argument and creates local struct timespec* tp; 
  * Then it sets tp->tv_sec = input_arg / 1000
  * and tp->tv_nsec = (input_arg % 1000) * 1000000.
  * At last it loads 0 (NULL) as second argument to nanosleep,
  * calls nanosleep(tp, NULL) and returns;
+ * If we would want program to sleep for 1 second,
+ * we should call gfx_sleep(1000);
  */
 void gfx_sleep(long arg);
 
 /** 
- * Calls clock_getttime(NULL, tp) where tp is timespec* struct.
+ * Calls clock_gettime(NULL, tp) where tp is timespec* struct.
  * after retrieving the information, it calculates time in milliseconds
  * converting read seconds and nanoseconds to milliseconds:
  * eg. tp->tv_sec * 1000 + tp->tv_nsec / 1000000
